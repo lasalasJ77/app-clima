217fe32c57017433a50b59203b9116e6b68b89fc
JoelL77 <joellasalas77@gmail.com>
2024-08-06 11:18:13 -0300

    add environments

--- src/app/api/services/clima/clima.service.ts
+++ src/app/api/services/clima/clima.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -9,12 +10,11 @@ import { Observable } from 'rxjs';
 export class ClimaService {
 
   url = 'http://api.openweathermap.org/data/2.5/weather?&appid=';
-  key = '64c0e6cbc66bc5ab72f8e2c9871be556';
 
   constructor(private http: HttpClient) { }
 
   getClima(ciudad: string): Observable<any> {
-    const URL = this.url + this.key + '&q='+ ciudad;
+    const URL = this.url + environment.api_cloud + '&q='+ ciudad;
     return this.http.get(URL)
   }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    api_cloud : "64c0e6cbc66bc5ab72f8e2c9871be556"
+};
